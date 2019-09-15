19e7d149edc661a0a6d115e6cf979bd6d72bff34
hayashiki <hayashida.masayuki.2nd@gmail.com>
2019-09-15 16:01:10 +0900

    Add model spec

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   it 'has a valid factory' do
     expect(create :task).to be_valid
-  end  
+  end 
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(500) }
+  end
 end
